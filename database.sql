0c822a10bfb717bad55590d1f56f847b81a895df
Rishi Suresh <rishi9632@gmail.com>
2023-05-27 14:00:44 +0530

    master: Add clean pg dump

--- database.sql
+++ database.sql
@@ -1,48 +1,154 @@
+DROP SCHEMA IF EXISTS poushak CASCADE;
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.1 (Debian 13.1-1.pgdg100+1)
+-- Dumped by pg_dump version 13.1 (Debian 13.1-1.pgdg100+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: poushak; Type: SCHEMA; Schema: -; Owner: admin
+--
+
 CREATE SCHEMA IF NOT EXISTS poushak;
 
-SET search_path=poushak;
 
-CREATE TABLE IF NOT EXISTS users(
-	id varchar(50) NOT NULL,
-	first_name varchar(50) DEFAULT '',
-	last_name varchar(50) DEFAULT '',
-	dob date DEFAULT current_date,
-	gender varchar(15) DEFAULT '',
-	aadhar varchar(200) UNIQUE,
-	pan varchar(200) UNIQUE,
-	email varchar(100) UNIQUE,
-	fb_email varchar(100) UNIQUE,
-	phone varchar(15) UNIQUE,
-	password varchar(200) DEFAULT '',
-	default_address varchar(50),
-	deleted boolean DEFAULT false,
-	verified boolean DEFAULT false,
-	t_and_c boolean DEFAULT false,
-	created_at timestamptz DEFAULT current_timestamp,
-	updated_at timestamptz DEFAULT current_timestamp,
-	CONSTRAINT users_pkey PRIMARY KEY(id)
-)
+ALTER SCHEMA poushak OWNER TO admin;
 
-CREATE TABLE IF NOT EXISTS addresses(
-	id varchar(50) NOT NULL,
-	user_id varchar(50),
-	name varchar(50),
-	phone varchar(15),
-	first_line varchar(100),
-	second_line varchar(100),
-	address_string varchar(300),
-	city varchar(50),
-	state varchar(50),
-	country varchar(50),
-	pincode varchar(6),
-	lat numeric(8, 6) NOT NULL,
-	long numeric(8, 6) NOT NULL,
-	deleted boolean DEFAULT false,
-	created_at timestamptz DEFAULT current_timestamp,
-	updated_at timestamptz DEFAULT current_timestamp,
-	CONSTRAINT addresses_pkey PRIMARY KEY(id),
-	CONSTRAINT addresses_users_fkey FOREIGN KEY(user_id) REFERENCES users(id)
-)
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: addresses; Type: TABLE; Schema: poushak; Owner: admin
+--
+
+CREATE TABLE IF NOT EXISTS poushak.addresses (
+    id character varying(50) NOT NULL,
+    user_id character varying(50),
+    first_line character varying(100),
+    second_line character varying(100),
+    address_string character varying(300),
+    city character varying(50),
+    state character varying(50),
+    country character varying(50),
+    pincode character varying(6),
+    lat numeric(8,6) NOT NULL,
+    long numeric(8,6) NOT NULL,
+    deleted boolean DEFAULT false,
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    name character varying(50),
+    phone character varying(15),
+    is_default boolean DEFAULT false
+);
 
 
-ALTER TABLE users ADD CONSTRAINT users_addresses_fkey FOREIGN KEY(default_address) REFERENCES addresses(id)
+ALTER TABLE poushak.addresses OWNER TO admin;
+
+--
+-- Name: users; Type: TABLE; Schema: poushak; Owner: admin
+--
+
+CREATE TABLE IF NOT EXISTS poushak.users (
+    id character varying(50) NOT NULL,
+    first_name character varying(50) DEFAULT ''::character varying,
+    last_name character varying(50) DEFAULT ''::character varying,
+    dob date DEFAULT CURRENT_DATE,
+    gender character varying(15) DEFAULT ''::character varying,
+    aadhar character varying(200),
+    pan character varying(200),
+    email character varying(100),
+    fb_email character varying(100),
+    phone character varying(15),
+    password character varying(200) DEFAULT ''::character varying,
+    default_address character varying(50),
+    deleted boolean DEFAULT false,
+    t_and_c boolean DEFAULT false,
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+    phone_verified boolean DEFAULT false,
+    email_verified boolean DEFAULT false
+);
+
+
+ALTER TABLE poushak.users OWNER TO admin;
+
+--
+-- Name: addresses addresses_pkey; Type: CONSTRAINT; Schema: poushak; Owner: admin
+--
+
+ALTER TABLE ONLY poushak.addresses
+    ADD CONSTRAINT addresses_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_aadhar_key; Type: CONSTRAINT; Schema: poushak; Owner: admin
+--
+
+ALTER TABLE ONLY poushak.users
+    ADD CONSTRAINT users_aadhar_key UNIQUE (aadhar);
+
+
+--
+-- Name: users users_email_key; Type: CONSTRAINT; Schema: poushak; Owner: admin
+--
+
+ALTER TABLE ONLY poushak.users
+    ADD CONSTRAINT users_email_key UNIQUE (email);
+
+
+--
+-- Name: users users_fb_email_key; Type: CONSTRAINT; Schema: poushak; Owner: admin
+--
+
+ALTER TABLE ONLY poushak.users
+    ADD CONSTRAINT users_fb_email_key UNIQUE (fb_email);
+
+
+--
+-- Name: users users_pan_key; Type: CONSTRAINT; Schema: poushak; Owner: admin
+--
+
+ALTER TABLE ONLY poushak.users
+    ADD CONSTRAINT users_pan_key UNIQUE (pan);
+
+
+--
+-- Name: users users_phone_key; Type: CONSTRAINT; Schema: poushak; Owner: admin
+--
+
+ALTER TABLE ONLY poushak.users
+    ADD CONSTRAINT users_phone_key UNIQUE (phone);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: poushak; Owner: admin
+--
+
+ALTER TABLE ONLY poushak.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: addresses addresses_users_fkey; Type: FK CONSTRAINT; Schema: poushak; Owner: admin
+--
+
+ALTER TABLE ONLY poushak.addresses
+    ADD CONSTRAINT addresses_users_fkey FOREIGN KEY (user_id) REFERENCES poushak.users(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
